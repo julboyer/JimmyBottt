489c6eca6253d5b801b6466570a5225ec8a2b05f
Julien Boyer <julienboyer@hotmail.fr>
2025-01-08 01:56:24 +0100

    added a dockerfile for the bot itself

---
+++ requirements/database/script/db-init.sh
@@ -0,0 +1,11 @@
+service mariadb start;
+#environement variables will be passed through a .env file
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+#identifie l'utilisateur créé juste avant comme l'administrateur
+mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+mysql -e "FLUSH PRIVILEGES;"
+sleep 1;
+mysqladmin -u root shutdown
+exec mysqld_safe
