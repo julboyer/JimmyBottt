7adfd7d5359b7210064325395fc977ab5b712d76
Julien Boyer <julienboyer@hotmail.fr>
2025-01-21 12:00:59 +0100

    script creates tables properly

--- requirements/database/script/db-init.sh
+++ requirements/database/script/db-init.sh
@@ -1,26 +1,23 @@
 service mariadb start;
 #environement variables will be passed through a .env file
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-# echo "1"
-mysql -e "USE \`${SQL_DATABASE}\`;"
-# echo "2"
-mysql -e "CREATE TABLE IF NOT EXISTS TICKETS (
-	ticketsID INT AUTO INCREMENT UNIQUE NOT NULL,
+#creates necessary tables this needs to be done in a single request as mysql won't save which database you're using otherwise
+mysql -e "USE \`${SQL_DATABASE}\`;
+	CREATE TABLE IF NOT EXISTS TICKETS (
+	ticketsID INT AUTO_INCREMENT UNIQUE NOT NULL,
 	channelID INT UNIQUE NOT NULL,
 	userID INT NOT NULL,
 	logFile VARCHAR(32) NOT NULL,
 	PRIMARY KEY(ticketsID)
-	);"
-
-	mysql -e "CREATE TABLE IF NOT EXISTS ATTACHMENTS (
-    attachmentID INT AUTO INCREMENT UNIQUE NOT NULL,
+	);
+	CREATE TABLE IF NOT EXISTS ATTACHMENTS (
+    attachmentID INT AUTO_INCREMENT UNIQUE NOT NULL,
     attachementFilename VARCHAR(32),
-    ticketsID INT REFERENCES TICKETS.ticketsID,
-    PRIMARY KEY(attachmentID),
-);"
-# echo "3"
+	ticketsID INT,
+    FOREIGN KEY (ticketsID) REFERENCES TICKETS (ticketsID),
+    PRIMARY KEY(attachmentID)
+	);"
 mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-# echo "4"
 #identifie l'utilisateur créé juste avant comme l'administrateur
 mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
