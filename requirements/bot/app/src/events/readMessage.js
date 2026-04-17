783fe890d124c2a2f1a7c6ac5ad23189eeb8e050
François "Kurikawa" Odin <francois@kurikawa.fr>
2026-04-17 12:08:01 +0200

    autoban + purge if a user writes in a specific channel

--- requirements/bot/app/src/events/readMessage.js
+++ requirements/bot/app/src/events/readMessage.js
@@ -2,8 +2,19 @@ const {Events} = require('discord.js');
 
 module.exports = {
 	name:Events.MessageCreate,
+	autobanChannelId: "",
+	purgeMessageTime: 60, // temps en secondes
 	async execute(message){
-		console.log(message);
-		
+		if (message.channelId == this.autobanChannelId) {
+
+			targetMember = await message.guild.members.fetch(message.author.id);
+
+			hammer = {
+				reason:"Autodetection d'un compte spam",
+				deleteMessageSeconds: this.purgeMessageTime
+			};
+
+			if (targetMember && targetMember.bannable) await targetMember.ban(hammer);
+		}	
 	}
 };
